16df3ebd179dedb84e4b47ed34cde4733ad09f60
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2014-08-26 00:00:00 +0000

    version 0.8-7

---
+++ vignettes/AQuickStart.R
@@ -0,0 +1,24 @@
+### R code from vignette source 'AQuickStart.Rnw'
+
+###################################################
+### code chunk number 1: AQuickStart.Rnw:38-46
+###################################################
+
+library(gMCP)
+options(width=140)
+options(prompt=" ", continue=" ")
+options(digits=4)
+gMCPReport <- function(...) {invisible(NULL)}
+graphGUI <- function(...) {invisible(NULL)}
+
+
+
+###################################################
+### code chunk number 2: AQuickStart.Rnw:77-81
+###################################################
+
+library(gMCP)
+graphGUI()
+
+
+
